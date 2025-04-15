0c2645ef8d166da8e69bb72c15f5aa60ca72282d
ericklpps <ericklpsilva@gmail.com>
2025-04-15 09:59:31 -0300

    ajustando funcionalidades e estilizando melhor o tabuleiro

--- js/app.js
+++ js/app.js
@@ -1,13 +1,79 @@
-let celulasTabuleiro = document.querySelectorAll(".celula-tabuleiro");
-let tabuleiro = document.getElementById("tabuleiro");
-let statusJogo = document.getElementById("situacao-jogo");
+const jogadorAtual = document.querySelector('.jogador-atual');
+const celulas = document.querySelectorAll('.celula-tabuleiro');
+const botaoReiniciar = document.getElementById('reiniciar');
 
-document.addEventListener(celulasTabuleiro)
+let jogador = 'X';
+let selecionado = [];
 
-function capturaClique(e){    
-    console.log("célula clicada")
+const contextosVitoria = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+function iniciarJogo() {
+  selecionado = [];
+  jogador = 'X';
+  jogadorAtual.textContent = `Jogador da vez: ${jogador}`;
+
+  celulas.forEach((celula) => {
+    celula.textContent = '';
+    celula.disabled = false;
+    celula.classList.remove('vencedor');
+  });
+  celulas.forEach((celula, indice) => {
+    celula.addEventListener('click', () => novaJogada(celula, indice), { once: true });
+  });
 }
 
-celulasTabuleiro.forEach((umaCelula) => { 
-    umaCelula.addEventListener('click', capturaClique); 
-});
+function novaJogada(celula, indice) {
+  if (celula.textContent !== '') return; 
+
+  celula.textContent = jogador;
+  celula.disabled = true;
+  selecionado[indice] = jogador;
+
+  if (verificarVencedor()) {
+    encerrarJogo(`Jogador '${jogador}' venceu!`);
+    return;
+  }
+
+  if (selecionado.filter(Boolean).length === 9) {
+    encerrarJogo('Empate!');
+    return;
+  }
+
+  jogador = jogador === 'X' ? 'O' : 'X';
+  jogadorAtual.textContent = `Jogador da vez: ${jogador}`;
+}
+
+function verificarVencedor() {
+  return contextosVitoria.some((sequencia) => {
+    const venceu = sequencia.every((indice) => selecionado[indice] === jogador);
+    if (venceu) {
+      sequencia.forEach((indice) => {
+        celulas[indice].classList.add('vencedor');
+      });
+    }
+    return venceu;
+  });
+}
+
+function encerrarJogo(mensagem) {
+  jogadorAtual.textContent = mensagem;
+
+  celulas.forEach((celula) => {
+    celula.disabled = true;
+  });
+
+  setTimeout(iniciarJogo, 2000);
+}
+
+botaoReiniciar.addEventListener('click', iniciarJogo);
+
+iniciarJogo();
